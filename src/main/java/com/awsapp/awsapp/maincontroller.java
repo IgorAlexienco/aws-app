e3e83abf30ad9f84725d2f4ac4a0c5b19f9f0c3c
IgorGritsanchouk <igor.gritsanchouk@gmail.com>
2022-05-23 12:01:13 +0100

    added 4th location to the controller

--- src/main/java/com/awsapp/awsapp/maincontroller.java
+++ src/main/java/com/awsapp/awsapp/maincontroller.java
@@ -16,10 +16,12 @@ public class maincontroller {
         location l1= new location("location 111", "123 Main St", 31.01, -121.11);
         location l2= new location("location 222", "444 22 nn St", 32.02, -122.22);
         location l3= new location("location 333", "888 Main St", 33.03, -123.33);
+        location l4= new location("location 444", "4 Chapel Arches", 44.04, -444.44);
 
         requestedLocations.add(l1);
         requestedLocations.add(l2);
         requestedLocations.add(l3);
+        requestedLocations.add(l4);
 
         return requestedLocations;
     }
